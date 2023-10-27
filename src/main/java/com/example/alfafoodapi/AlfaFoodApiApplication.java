2f8c31c374a4dcc439aeb8af21f94b3d6aa2e6a2
wellerson <wellersonjava@gmail.com>
2023-10-27 17:43:37 -0300

    Criação da api, gerção do git inilize

---
+++ src/main/java/com/example/alfafoodapi/AlfaFoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.alfafoodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlfaFoodApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlfaFoodApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/alfafoodapi/AlfaFoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alfafoodapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlfaFoodApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
